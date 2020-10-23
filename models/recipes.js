550d8f2f62118f35a678c19df3db628b311525e0
Sean Kramer <seandominickramer@gmail.com>
2020-10-22 21:27:15 -0400

    Add endpoint to ge a recipe by id

--- models/recipes.js
+++ models/recipes.js
@@ -4,6 +4,13 @@ function find() {
   return db('recipes')
 }
 
+function findById(id) {
+  return db('recipes')
+  .where('id', id)
+  .first()
+}
+
 module.exports = {
   find,
+  findById,
 }
--- routers/recipes.js
+++ routers/recipes.js
@@ -12,5 +12,19 @@ res.json(recipes)
   }
 })
 
+router.get('/recipes/:id', async (req, res, next) => {
+  try {
+const recipe = await Recipe.findById(req.params.id)
+if (!recipe) {
+  return res.status(404).json({
+    message: "Recipe not found"
+  })
+}
+res.json(recipe)
+  } catch(err) {
+    next(err)
+  }
+})
+
 
 module.exports = router
